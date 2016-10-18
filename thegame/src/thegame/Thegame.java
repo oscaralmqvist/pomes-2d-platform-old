ba6584ff08263dd8bc5c4959cb75d588af38de76
oscaralmqvist <Elev@10.100.11.1>
2016-10-18 16:32:13 +0200

    konstrukor

--- thegame/src/thegame/Thegame.java
+++ thegame/src/thegame/Thegame.java
@@ -6,6 +6,12 @@ package thegame;
  * @author Oscar Almqvist
  */
 public class Thegame {
+    private int x;
+    
+    
+    public Thegame(int x){
+        this.x = x;
+    }
 
     public static void main(String[] args) {
         System.out.println("S");
